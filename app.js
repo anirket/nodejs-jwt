a4bc3675ec1ce3f47c4642f787f8d665f6b26d0d
Aniket <aniket.kulkarni@smallcase.com>
2022-07-20 09:58:37 +0530

    ready to deploy

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(cookieparser());
 app.use(cors());
 
-app.listen(4000);
+app.listen(process.env.PORT || 4000);
 const circulardata = require('./Circular.json')
 
 
